026e195b0874713b48de2ee5fe2d6a6ca1dfd4d3
Anwesh43 <anweshthecool0@gmail.com>
2017-12-28 22:33:57 +0530

    adding MeterRotator

--- js-anim-meter.js
+++ js-anim-meter.js
@@ -7,10 +7,13 @@ class MeterHolder {
         const colors = this.colors
         if(colors.length > 0) {
             const sweep = (180)/colors.length  - 5
+            context.save()
+            context.translate(size,size)
             for(var i=0;i<colors.length;i++) {
                 context.fillStyle = colors[i]
                 this.drawArcShape(context,180+i*start,sweep)
             }
+            context.restore()
         }
     }
     drawArcShape(context,start,sweep) {
@@ -25,5 +28,31 @@ class MeterHolder {
             }
             context.fill()
         }
+    }
+}
+class MeterRotator {
+    constructor() {
+
+    }
+    draw(context) {
+        context.save()
+        context.fillStyle = 'white'
+        context.translate(size,size)
+        context.beginPath()
+        context.arc(0,0,size,Math.PI,2*Math.PI)
+        context.fill()
+        context.fillStyle = 'black'
+        context.beginPath()
+        context.arc(0,-size/3,size/3,0,Math.PI)
+        context.lineTo(0,-size/2)
+        context.lineTo(size/3,-size/3)
+        context.fill()
+        context.restore()
+    }
+    update(stopcb) {
+
+    }
+    startUpdating(deg,startcb) {
+
     }
 }
